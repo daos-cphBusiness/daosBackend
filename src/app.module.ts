042a78bc277b8ea16532330cf261f535c2ea56ad
Orestis Mamouzelos <ores0029@stud.kea.dk>
2024-11-21 12:50:59 +0100

    orestis' first attempt to link ensembles to users

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { EnsembleModule } from './ensemble/ensemble.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/daos'),
     UsersModule,
     AuthModule,
+    EnsembleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
